37032d92462b295712c26f4efc240d71c2f5857e
pitufo08 <camilo.vasquez82@gmail.com>
2020-07-01 17:19:04 +0000

    changing heroku name app information to the right one.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['pitufo-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pitufo08-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
