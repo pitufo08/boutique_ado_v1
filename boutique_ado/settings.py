70bfcc9f6398bce55e7a063ae01d4fa8be72f4cc
pitufo08 <camilo.vasquez82@gmail.com>
2020-07-02 05:35:12 +0000

    correcting a syntax error on settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
 
-    #Other
+    # Other
     'crispy_forms',
 ]
 
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default' : dj_database_url.parse(os.environ.get('DATABASE_URL)'))
+        'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else: 
     DATABASES = {
